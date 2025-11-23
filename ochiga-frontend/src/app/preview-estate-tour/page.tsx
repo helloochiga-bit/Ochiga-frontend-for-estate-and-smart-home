19363b2dd47f5770c61a75269be874ea0e9186ec
helloochiga-bit <helloochiga@gmail.com>
2025-11-23 08:09:19 +0100

    Update page.tsx

--- ochiga-frontend/src/app/preview-estate-tour/page.tsx
+++ ochiga-frontend/src/app/preview-estate-tour/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import EstateTour from "../estate-dashboard/components/EstateTour";
+import EstateTour from "../estate-dashboard/components/Onboarding/EstateTour";
 
 export default function PreviewEstateTour() {
   return <EstateTour />;
