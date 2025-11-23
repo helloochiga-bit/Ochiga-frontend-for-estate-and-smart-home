edebbf376333aedc435366d295c26704f1300132
helloochiga-bit <helloochiga@gmail.com>
2025-11-23 11:45:46 +0100

    Create index.ts

---
+++ ochiga-frontend/src/app/estate-dashboard/components/panels/index.ts
@@ -0,0 +1,6 @@
+// src/app/estate-dashboard/components/panels/index.ts
+export { default as EstateDevicePanel } from "./EstateDevicePanel";
+export { default as EstatePowerPanel } from "./EstatePowerPanel";
+export { default as EstateAccountingPanel } from "./EstateAccountingPanel";
+export { default as EstateCommunityPanel } from "./EstateCommunityPanel";
+export { default as EstateHomeCreationPanel } from "./EstateHomeCreationPanel";
