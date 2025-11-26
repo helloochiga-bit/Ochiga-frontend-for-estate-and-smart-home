c21830bfd71d5e133d8ebaeaef1b783cd60c1a7f
helloochiga-bit <helloochiga@gmail.com>
2025-11-26 18:47:17 +0100

    Update authApi.ts

--- ochiga-frontend/src/services/authApi.ts
+++ ochiga-frontend/src/services/authApi.ts
@@ -1,5 +1,3 @@
-// src/services/authApi.ts
-
 const BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
 
@@ -7,7 +5,7 @@ export async function loginApi(email: string, password: string) {
   const res = await fetch(`${BASE_URL}/auth/login`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ email, password }),
+    body: JSON.stringify({ usernameOrEmail: email, password }),
     credentials: "include",
   });
 
